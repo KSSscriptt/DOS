51c11367bce9f20b3ea9541831e0d88e850370a8
jusifake <102470125+KSSscriptt@users.noreply.github.com>
2022-03-30 11:07:32 +0600

    Create Termux-commands.sh

---
+++ Termux-commands.sh
@@ -0,0 +1,10 @@
+apt update
+apt upgrade
+pkg install python
+pip install colorama
+pip install threading
+pip install requests
+pkg install git
+git clone https://github.com/KSSscriptt/DOS
+cd DOS
+python3 attack.py
